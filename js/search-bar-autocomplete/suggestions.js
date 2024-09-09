dc187833b3578c93cdfb0f83a1e40da1e40b873a
Furkan TURAN <turan.cmd@gmail.com>
2024-09-10 00:08:43 +0300

    Folders uploaded.

---
+++ js/search-bar-autocomplete/suggestions.js
@@ -0,0 +1,18 @@
+let suggestions = [
+    "Anakart Wallpaper",
+    "Black Wallpaper",
+    "Blue Keyboard Wallpaper",
+    "Coding Symbols Wallpaper",
+    "Cyber Security Wallpaper",
+    "Data Wallpaper",
+    "Desktop Wallpaper",
+    "Hello World Wallpaper",
+    "Html Code Wallpaper",
+    "Keyboard Wallpaper",
+    "Mackbook Code Wallpaper",
+    "Ryzen CPU Wallpaper",
+    "Satellite Wallpaper",
+    "Server Wallpaper",
+    "Windows 10 Wallpaper",
+    "",
+];
